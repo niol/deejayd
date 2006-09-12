6ad80e57b5790047e4cbf9503f5a5935afc5c4ef
Alexandre Rossi <alexandre.rossi@gmail.com>
2006-09-13 00:27:39 +0200

    deejayd test cases base classes
    darcs-hash:20060912222739-85ee7-622d98534fa531ffc0f04c2de838c4e1f05dc072.gz
    
    committer: Alexandre Rossi <alexandre.rossi@gmail.com>

--- testdeejayd/__init__.py
+++ testdeejayd/__init__.py
@@ -1,6 +1,21 @@
+import unittest
 from testdeejayd.databuilder import TestMusicCollection
 import testdeejayd.data
 
-testdata = TestMusicCollection(testdeejayd.data.songlibrary)
+class TestCaseWithData(unittest.TestCase):
+
+    def setUp(self):
+        self.testdata = TestMusicCollection(testdeejayd.data.songlibrary)
+
+
+class TestCaseWithFileData(TestCaseWithData):
+
+    def setUp(self):
+        TestCaseWithData.__init__(self)
+        self.testdata.buildLibraryDirectoryTree('/tmp')
+
+    def tearDown(self):
+        self.testdata.cleanLibraryDirectoryTree()
+
 
 # vim: ts=4 sw=4 expandtab
