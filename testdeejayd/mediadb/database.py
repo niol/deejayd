d8ea311f80040f8917684d04d3b7b18e3902bdaa
Alexandre Rossi <alexandre.rossi@gmail.com>
2006-09-13 00:28:49 +0200

    sample test added - runs but fails ;)
    darcs-hash:20060912222849-85ee7-d90817033003bc0bc5a3ea578916aea7bfa4e8d8.gz
    
    committer: Alexandre Rossi <alexandre.rossi@gmail.com>

---
+++ testdeejayd/mediadb/database.py
@@ -0,0 +1,24 @@
+from testdeejayd import TestCaseWithData
+from deejayd.mediadb.database import sqliteDatabase
+import os
+
+class testSqliteDatabase(TestCaseWithData):
+
+    def setUp(self):
+        TestCaseWithData.setUp(self)
+
+        self.dbfilename = '/tmp/testdeejayddb-' + self.testdata.getRandomString()
+        self.db = sqliteDatabase(self.dbfilename)
+        self.db.connect()
+
+    def tearDown(self):
+        TestCaseWithData.tearDown(self)
+
+        os.remove(self.dbfilename)
+
+    def testGetUnexistentPlaylist(self):
+        randomName = self.testdata.getRandomString()
+        self.assertRaises(PlaylistNotFound,self.db.getPlaylist(randomName))
+
+
+# vim: ts=4 sw=4 expandtab
--- tests.py
+++ tests.py
@@ -4,8 +4,10 @@ This is the test suite launcher.
 """
 
 import unittest
-import testdeejayd
 
-unittest.main()
+import testdeejayd.mediadb.database
+dbsuite = unittest.defaultTestLoader.loadTestsFromModule(testdeejayd.mediadb.database)
+
+unittest.TextTestRunner(verbosity=2).run(dbsuite)
 
 # vim: ts=4 sw=4 expandtab
