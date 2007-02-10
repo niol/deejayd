846a9f37a6090da5bbb49c398c8555f4f6ccbb80
Mickael ROYER <mickael.royer@gmail.com>
2007-02-10 23:59:54 +0100

    package deejayd with python distutils (setup.py file)
    darcs-hash:20070210225954-73b03-be8fcb4d511d1715e7cc977bc5940dca65817eca.gz
    
    committer: Mickael ROYER <mickael.royer@gmail.com>

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import glob
+import os
+
+import shutil
+import sys
+
+from distutils.core import setup, Command
+from distutils.command.clean import clean as distutils_clean
+from distutils.command.sdist import sdist as distutils_sdist
+
+if __name__ == "__main__":
+    #setup(cmdclass={'clean': clean, 'test': test_cmd, 'coverage': coverage_cmd,
+    #                "sdist": sdist, "release": release},
+    setup( name="deejayd", version="0.1.0",
+           url="http://mroy31.dyndns.org/~roy/projects/deejayd",
+           description="",
+           author="Mikael Royer",
+           author_email="mickael.royer@gmail.com",
+           license="GNU GPL v2",
+           scripts=["scripts/deejayd"],
+           packages=["deejayd","deejayd.net","deejayd.mediadb",\
+                     "deejayd.player","deejayd.sources","deejayd.ui"],
+           package_data={'deejayd.ui': ['defaults.conf']},
+           data_files=[('share/doc/deejayd-0.1.0', glob.glob("doc/*"))])
+           #data_files=[('share/doc/deejayd-0.1.0', glob.glob("doc/*")),
+                       #('etc/deejayd.conf',"conf/deejayd.conf"))])
