c0c083148c80cb5657602ad365a2649cf9b33cf5
Mickael ROYER <mickael.royer@gmail.com>
2007-02-11 12:11:08 +0100

    clean up setup.py
    darcs-hash:20070211111108-73b03-90436d8d627854eba3d8739bba7d62633912b592.gz
    
    committer: Mickael ROYER <mickael.royer@gmail.com>

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
 #!/usr/bin/env python
 
 import glob
-import os
-
-import shutil
-import sys
-
-from distutils.core import setup, Command
-from distutils.command.clean import clean as distutils_clean
-from distutils.command.sdist import sdist as distutils_sdist
+from distutils.core import setup
 import deejayd
 
 if __name__ == "__main__":
-    #setup(cmdclass={'clean': clean, 'test': test_cmd, 'coverage': coverage_cmd,
-    #                "sdist": sdist, "release": release},
     setup( name="deejayd", version=deejayd.__version__,
            url="http://mroy31.dyndns.org/~roy/projects/deejayd",
-           description="",
+           description="deejayd is a media player daemon based on twisted",
            author="Mikael Royer",
            author_email="mickael.royer@gmail.com",
            license="GNU GPL v2",
@@ -24,6 +15,6 @@ if __name__ == "__main__":
            packages=["deejayd","deejayd.net","deejayd.mediadb",\
                      "deejayd.player","deejayd.sources","deejayd.ui"],
            package_data={'deejayd.ui': ['defaults.conf']},
-           data_files=[('share/doc/deejayd-0.1.0', glob.glob("doc/*"))])
-           #data_files=[('share/doc/deejayd-0.1.0', glob.glob("doc/*")),
-                       #('etc/deejayd.conf',"conf/deejayd.conf"))])
+           data_files=[('share/doc/deejayd-'+deejayd.__version__, 
+                            glob.glob("doc/*")),
+                       ('share/doc/deejayd-0.1.0',glob.glob("README*"))])
