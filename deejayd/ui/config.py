3a3e9d00751b6e34d8e57ab523e651568beb1a35
Mickael ROYER <mickael.royer@gmail.com>
2007-02-17 16:27:11 +0100

    add an option to choose a custom configuration file
    darcs-hash:20070217152711-73b03-9152fa9dd9bbe8b1758c6f6027305ef173bf1230.gz
    
    committer: Mickael ROYER <mickael.royer@gmail.com>

--- deejayd/ui/config.py
+++ deejayd/ui/config.py
@@ -2,6 +2,9 @@ import ConfigParser, os, sys
 
 class DeejaydConfig:
 
+    customConf = None
+    __globalConf = '/etc/deejayd.conf'
+    __userConf = '~/.deejayd.conf'
     __config = None
 
     def __init__(self):
@@ -10,11 +13,14 @@ class DeejaydConfig:
             DeejaydConfig.__config = ConfigParser.ConfigParser()
 
             defaultConfigPath = os.path.abspath(os.path.dirname(__file__))
-            DeejaydConfig.__config.readfp(open(defaultConfigPath
+            DeejaydConfig.__config.readfp(open(defaultConfigPath\
                                         + '/defaults.conf'))
 
-            DeejaydConfig.__config.read(['/etc/deejayd.conf',
-                                    os.path.expanduser('~/.deejayd.conf')])
+            confFiles = [DeejaydConfig.__globalConf,\
+                         os.path.expanduser(DeejaydConfig.__userConf)]
+            if DeejaydConfig.customConf:
+                confFiles.append(DeejaydConfig.customConf)
+            DeejaydConfig.__config.read(confFiles)
 
     def __getattr__(self, name):
         return getattr(DeejaydConfig.__config, name)
