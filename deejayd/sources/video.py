20b5ca1b3586a9f100c3531c91e71ccf9200fc32
Mickael ROYER <mickael.royer@gmail.com>
2007-03-25 22:29:55 +0200

    minor fixes in video mode
    darcs-hash:20070325202955-73b03-13f7df30547e442ff33c173c7f64cff6aa4a01b9.gz
    
    committer: Mickael ROYER <mickael.royer@gmail.com>

--- deejayd/sources/video.py
+++ deejayd/sources/video.py
@@ -18,7 +18,7 @@ class Video(UnknownSource):
         for f in files:
             pos = initPos+i
             self.sourceContent.append({"dir":f[0],"Title":f[4],"filename":f[1],\
-                "Pos":pos,"Id":f[2],"uri":"file://"+path.join(self.rootPath,\
+                "Pos":pos,"Id":f[3],"uri":"file://"+path.join(self.rootPath,\
                 path.join(f[0],f[1])),"Time":f[5],"Res":f[6]})
             i += 1
 
@@ -45,6 +45,8 @@ class VideoSource(UnknownSourceManagement):
 
     def setDirectory(self,dir):
         self.currentSource.clear()
+        self.currentItem = None
+
         try: videoList = self.djDB.getVideoFiles(dir)
         except NotFoundException: videoList = []
 
