1a0ea5163e25961c5c59b207fc504bfcf840bf26
Alexandre Rossi <alexandre.rossi@gmail.com>
2007-04-09 17:03:57 +0200

    xmlbuilders now build all answers and generate doc
    darcs-hash:20070409150357-85ee7-3857640160518ecf6d17c815f0f0394c1b874721.gz
    
    committer: Alexandre Rossi <alexandre.rossi@gmail.com>

--- deejayd/net/xmlbuilders.py
+++ deejayd/net/xmlbuilders.py
@@ -1,9 +1,20 @@
 from xml.dom import minidom
 
 
-class DeejaydXMLCommand:
+class DeejaydXML:
+
+    def __init__(self):
+        self.xmldoc = minidom.Document()
+
+        # Add root
+        self.xmlroot = self.xmldoc.createElement('deejayd')
+        self.xmldoc.appendChild(self.xmlroot)
+
+
+class DeejaydXMLCommand(DeejaydXML):
 
     def __init__(self, name):
+        DeejaydXML.__init__(self)
         self.name = name
         self.args = {}
 
@@ -14,20 +25,14 @@ class DeejaydXMLCommand:
         self.addSimpleArg(name, valuelist)
 
     def toXML(self):
-        xmldoc = minidom.Document()
-
-        # Add root
-        xmlroot = xmldoc.createElement('deejayd')
-        xmldoc.appendChild(xmlroot)
-
         # Add command
-        xmlcmd = xmldoc.createElement('command')
+        xmlcmd = self.xmldoc.createElement('command')
         xmlcmd.setAttribute('name', self.name)
-        xmlroot.appendChild(xmlcmd)
+        self.xmlroot.appendChild(xmlcmd)
 
         # Add args
         for arg in self.args.keys():
-            xmlarg = xmldoc.createElement('arg')
+            xmlarg = self.xmldoc.createElement('arg')
             xmlarg.setAttribute('name', arg)
             xmlcmd.appendChild(xmlarg)
 
@@ -38,17 +43,232 @@ class DeejaydXMLCommand:
                 xmlarg.setAttribute('type', 'multiple')
 
                 for argParamValue in argParam:
-                    xmlval = xmldoc.createElement('value')
-                    xmlval.appendChild(xmldoc.createTextNode(
+                    xmlval = self.xmldoc.createElement('value')
+                    xmlval.appendChild(self.xmldoc.createTextNode(
                                 str(argParamValue) ))
                     xmlarg.appendChild(xmlval)
 
             else:
                 # We've got a simple arg
                 xmlarg.setAttribute('type', 'simple')
-                xmlarg.appendChild(xmldoc.createTextNode(str(argParam)))
+                xmlarg.appendChild(self.xmldoc.createTextNode(str(argParam)))
 
-        return xmldoc.toxml('utf-8')
+        return self.xmldoc.toxml('utf-8')
+
+
+class DeejaydXMLAnswer(DeejaydXML):
+
+    def __init__(self, originatingCmd):
+        DeejaydXML.__init__(self)
+        self.originatingCmd = originatingCmd
+
+    def __toXMLString(self, s):
+        if isinstance(s,int) or isinstance(s,float):
+            return "%d" % (s,)
+        elif isinstance(s,str):
+            return "%s" % (s)
+        elif isinstance(s,unicode):
+            return "%s" % (s.encode('utf-8'))
+
+    def buildXMLParm(self, name, value):
+        xmlparm = self.xmldoc.createElement('parm')
+        xmlparm.setAttribute('name', name)
+        xmlparm.setAttribute('value', self.__toXMLString(value))
+        return xmlparm
+
+    def buildXMLListparm(self, name, valueList):
+        xmlListParm = self.xmldoc.createElement('listparm')
+        for value in valueList:
+            xmlvalue = self.xmldoc.createElement('value')
+            value = self.__toXMLString(value)
+            xmlvalue.appendChild(xmldoc.createTextNode(value))
+            xmlListParm.appendChild(xmlValue)
+        return xmlListParm
+
+    def buildXMLParmList(self, data, parentElement):
+        for dataKey, dataValue in data.items():
+            xmlParm = None
+            if type(dataValue) is list:
+                xmlParm = self.buildXMLListparm(dataKey, dataValue)
+            else:
+                xmlParm = self.buildXMLParm(dataKey, dataValue)
+            parentElement.appendChild(xmlParm)
+
+    def buildXML(self):
+        raise NotImplementedError
+
+    def toXML(self):
+        self.buildXML()
+        return self.xmldoc.toxml('utf-8')
+
+    def toPrettyXML(self):
+        self.buildXML()
+        return self.xmldoc.toprettyxml(encoding = 'utf-8')
+
+
+class DeejaydXMLError(DeejaydXMLAnswer):
+    """Error notification."""
+
+    responseType = 'error'
+
+    def setErrorText(self, txt):
+        self.errorText = txt
+
+    def buildXML(self):
+        error = self.xmldoc.createElement(self.responseType)
+        error.setAttribute('name', self.originatingCmd)
+        error.appendChild(self.xmldoc.createTextNode(str(self.errorText)))
+        self.xmlroot.appendChild(error)
+        return self.xmldoc.toxml('utf-8')
+
+
+class DeejaydXMLAck(DeejaydXMLAnswer):
+    """Acknowledgement of a command."""
+
+    responseType = 'Ack'
+
+    def __init__(self, originatingCmd):
+        DeejaydXMLAnswer.__init__(self, originatingCmd)
+
+        self.response = self.xmldoc.createElement('response')
+        self.response.setAttribute('name', self.originatingCmd)
+
+    def buildXML(self):
+        self.response.setAttribute('type', self.responseType)
+        self.xmlroot.appendChild(self.response)
+
+
+class DeejaydXMLKeyValue(DeejaydXMLAck):
+    """A list of key, value pairs."""
+
+    responseType = 'KeyValue'
+
+    def __init__(self, originatingCmd):
+        DeejaydXMLAck.__init__(self, originatingCmd)
+        self.contents = {}
+
+    def addParm(self, key, value):
+        self.contents[key] = value
+
+    def setParms(self, keyValue):
+        self.contents = keyValue
+
+    def buildXML(self):
+        DeejaydXMLAck.buildXML(self)
+
+        for k, v in self.contents.items():
+            self.response.appendChild(self.buildXMLParm(k, v))
+
+
+class DeejaydXMLFileList(DeejaydXMLAck):
+    """A list of files and directories."""
+
+    responseType = 'FileList'
+
+    def __init__(self, originatingCmd):
+        DeejaydXMLAck.__init__(self, originatingCmd)
+        self.directories = []
+        self.files = []
+
+    def addDirectory(self, dirname):
+        self.directories.append(dirname)
+
+    def addFile(self, fileInfo):
+        self.files.append(fileInfo)
+
+    def buildXML(self):
+        DeejaydXMLAck.buildXML(self)
+
+        for dirname in self.directories:
+            xmldir = self.xmldoc.createElement('directory')
+            xmldir.setAttribute('name', dirname)
+            self.response.appendChild(xmldir)
+
+        for file in self.files:
+            xmlfile = self.xmldoc.createElement('file')
+            self.buildXMLParmList(file, xmlfile)
+            self.response.appendChild(xmlfile)
+
+
+class DeejaydWebradioList(DeejaydXMLAck):
+    """A list of webradios with information for each webradio : id, pos, title and url."""
+
+    responseType = 'WebradioList'
+
+    def __init__(self, originatingCmd):
+        DeejaydXMLAck.__init__(self, originatingCmd)
+        self.webradios = []
+
+    def addWebradio(self, wr):
+        self.webradios.append(wr)
+
+    def buildXML(self):
+        DeejaydXMLAck.buildXML(self)
+        for wr in self.webradios:
+            xmlwr = self.xmldoc.createElement('webradio')
+            self.buildXMLParmList(wr, xmlwr)
+            self.response.appendChild(xmlwr)
+
+
+class DeejaydXMLSongList(DeejaydXMLAck):
+    """A list of songs with information for each song : artist, album, title, id, etc."""
+
+    responseType = 'SongList'
+
+    def __init__(self, originatingCmd):
+        DeejaydXMLAck.__init__(self, originatingCmd)
+        self.songs = []
+
+    def addSong(self, song):
+        self.songs.append(song)
+
+    def buildXML(self):
+        DeejaydXMLAck.buildXML(self)
+        for song in self.songs:
+            xmlsong = self.xmldoc.createElement('song')
+            self.buildXMLParmList(song, xmlsong)
+            self.response.appendChild(xmlsong)
+
+
+class DeejaydPlaylistList(DeejaydXMLAck):
+    """A list of playlist names."""
+
+    responseType = 'PlaylistList'
+
+    def __init__(self, originatingCmd):
+        DeejaydXMLAck.__init__(self, originatingCmd)
+        self.playlistNames = []
+
+    def addPlaylist(self, playlistName):
+        self.playlistNames.append(playlistName)
+
+    def buildXML(self):
+        DeejaydXMLAck.buildXML(self)
+        for playlistName in self.playlistNames:
+            xmlpl = self.xmldoc.createElement('playlist')
+            xmlpl.setAttribute('name', playlistName)
+            self.response.appendChild(xmlpl)
+
+
+class DeejaydXMLAnswerFactory:
+
+    responseTypes = [ DeejaydXMLError,
+                      DeejaydXMLAck,
+                      DeejaydXMLKeyValue,
+                      DeejaydXMLFileList,
+                      DeejaydWebradioList,
+                      DeejaydXMLSongList,
+                      DeejaydPlaylistList ]
+
+    def getDeejaydXMLAnswer(self, type, originatingCmd):
+        iat = iter(self.responseTypes)
+        try:
+            while True:
+                typeClass = iat.next()
+                if typeClass.responseType == type:
+                    return typeClass(originatingCmd)
+        except StopIteration:
+            raise NotImplementedError
 
 
 # vim: ts=4 sw=4 expandtab
--- docs.py
+++ docs.py
@@ -4,6 +4,7 @@ Use to create documentation of the protocol
 """
 
 from deejayd.net import commandsXML,commandsLine
+from deejayd.net.xmlbuilders import *
 
 def headerLineCommands():
     return """
@@ -115,21 +116,7 @@ Response Format :
         <playlist name="pls2"/>
     </response>
 </deejayd>
------------------
-There are 6 response types
-  * "Ack" : just an acknowledgement of the command 
-  * "KeyValue" : a list of "key=value" couple  
-  * "FileList" : file and directory list
-  * "WebradioList" : webradio list with informations foreach webradio 
-                     (id,pos,title,url)
-  * "SongList" : song list (playlist content for example) with informations
-                 foreach song (artist,album,title,id...) 
-  * "PlaylistList" : playlist list 
-
--------------------------------------------------------------------------------
-Available Commands :
------------------
-    """
+-----------------"""
 
 def formatCmdDoc(name,cmdObj, xml = True):
     infos = cmdObj("",[]).docInfos()
@@ -162,10 +149,98 @@ description :
     }
 
 
+class DeejaydXMLDocFactory(DeejaydXMLAnswerFactory):
+
+    def getSampleParmDict(self, howMuch = 2):
+        parmDict = {}
+        for i in range(howMuch):
+            parmDict['parmName' + str(i)] = 'parmValue' + str(i)
+        return parmDict
+
+    def getError(self):
+        error = self.getDeejaydXMLAnswer('error', 'cmdName')
+        error.setErrorText('error text')
+        return error
+
+    def getAck(self):
+        ack = self.getDeejaydXMLAnswer('Ack', 'cmdName')
+        return ack
+
+    def getKeyValue(self):
+        kv = self.getDeejaydXMLAnswer('KeyValue', 'cmdName')
+        kv.setParms(self.getSampleParmDict())
+        return kv
+
+    def getFileList(self):
+        fl = self.getDeejaydXMLAnswer('FileList', 'cmdName')
+        fl.addDirectory('dirName')
+
+        fl.addFile(self.getSampleParmDict())
+
+        return fl
+
+    def getWebradioList(self):
+        wrl = self.getDeejaydXMLAnswer('WebradioList', 'cmdName')
+        wrl.addWebradio(self.getSampleParmDict())
+        return wrl
+
+    def getSongList(self):
+        sl = self.getDeejaydXMLAnswer('SongList', 'cmdName')
+        sl.addSong(self.getSampleParmDict())
+        return sl
+
+    def getPlaylistList(self):
+        pll = self.getDeejaydXMLAnswer('PlaylistList', 'getPlaylistList')
+        pll.addPlaylist('playlist1')
+        pll.addPlaylist('playlist2')
+        return pll
+
+    responseTypeExBuilders = { DeejaydXMLError: getError,
+                               DeejaydXMLAck: getAck,
+                               DeejaydXMLKeyValue: getKeyValue,
+                               DeejaydXMLFileList: getFileList,
+                               DeejaydWebradioList: getWebradioList,
+                               DeejaydXMLSongList: getSongList,
+                               DeejaydPlaylistList: getPlaylistList }
+ 
+    def getExample(self, responseClass):
+        builder = self.responseTypeExBuilders[responseClass]
+        if builder == None:
+            return 'No example available.'
+        else:
+            return builder(self)
+
+    def formatResponseDoc(self, response):
+        info = {}
+        info['type'] = response.responseType
+        info['desc'] = response.__doc__
+        info['example'] = self.getExample(response).toPrettyXML()
+
+        responseDoc = """
+  * %(type)s : %(desc)s
+{{{
+%(example)s}}}""" % info
+
+        return responseDoc
+
 if __name__ == "__main__":
     # XML Doc
     commandsListXML = commandsXML.commandsList(commandsXML)
     docs = headerXMLCommands()
+
+    docs += """
+There are 6 response types :"""
+
+    respDocBuilder = DeejaydXMLDocFactory()
+    for response in respDocBuilder.responseTypes:
+        docs += respDocBuilder.formatResponseDoc(response)
+
+    docs += """
+
+-------------------------------------------------------------------------------
+Available Commands :
+-----------------
+"""
     for cmd in commandsXML.commandsOrders():
         docs += formatCmdDoc(cmd,commandsListXML[cmd])
 
