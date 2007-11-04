0c3602791cf67dff3bda47a35fdfc7bed52a5085
Alexandre Rossi <alexandre.rossi@gmail.com>
2007-11-04 13:57:33 +0100

    improve prepare.sh
    darcs-hash:20071104125733-85ee7-55b5de11a07bfa2e3e356e3a202c0ad202550ac3.gz
    
    committer: Alexandre Rossi <alexandre.rossi@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -1,9 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-chmod +x src/scripts/testserver
-chmod +x src/debian/rules
+SRCDIR=$(dirname $0)
 
-chmod +x maemosrc/debian/rules
-cd maemosrc/deejayd/net
+makeexec (){
+    chmod +x $SRCDIR/$1
+}
+
+makeexec src/scripts/testserver
+makeexec src/tests.py
+makeexec src/debian/rules
+
+makeexec maemosrc/debian/rules
+
+cd $SRCDIR/maemosrc/deejayd/net
 ln -sf ../../../src/deejayd/net/client.py .
 ln -sf ../../../src/deejayd/net/xmlbuilders.py .
