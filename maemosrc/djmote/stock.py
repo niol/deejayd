66643d165f44d784deb2ca9f3cedd90f7d826721
Mickael Royer <mickael.royer@gmail.com>
2007-11-18 17:58:28 +0100

    [djmote] use tango media icons and reorganize pixmalder
    darcs-hash:20071118165828-b0006-6c7ef94600b72fccebb88ad274aad48942c2d00b.gz
    
    committer: Mickael Royer <mickael.royer@gmail.com>

--- maemosrc/djmote/stock.py
+++ maemosrc/djmote/stock.py
@@ -2,25 +2,36 @@
 import os
 import gtk
 
+# 72x72 icons
 DJMOTE_PLAY = "play_button"
 DJMOTE_PAUSE = "pause_button"
 DJMOTE_STOP = "stop_button"
 DJMOTE_NEXT = "forward_button"
 DJMOTE_PREVIOUS = "backward_button"
 DJMOTE_VOLUME = "volume_button"
+
+# 24x24 icons
 DJMOTE_SHUFFLE = "player-shuffle"
 DJMOTE_REPEAT = "player-repeat"
 
-_ICONS = [DJMOTE_PLAY,DJMOTE_PAUSE,DJMOTE_STOP,DJMOTE_NEXT,DJMOTE_PREVIOUS,\
-          DJMOTE_VOLUME,DJMOTE_SHUFFLE,DJMOTE_REPEAT]
+_ICONS_72 = [DJMOTE_PLAY,DJMOTE_PAUSE,DJMOTE_STOP,DJMOTE_NEXT,DJMOTE_PREVIOUS,\
+          DJMOTE_VOLUME]
+_ICONS_24 = [DJMOTE_SHUFFLE,DJMOTE_REPEAT]
 
 def init():
     factory = gtk.IconFactory()
     # Add new icons
     pixmaps_dir = os.path.join(os.path.abspath(os.path.dirname(__file__)),\
                                'pixmaps')
-    for fn in _ICONS:
-        icon_filename = os.path.join(pixmaps_dir, fn + ".png")
+    for fn in _ICONS_72:
+        dir = os.path.join(pixmaps_dir,"72x72")
+        icon_filename = os.path.join(dir, fn + ".png")
+        pb = gtk.gdk.pixbuf_new_from_file(icon_filename)
+        factory.add(fn, gtk.IconSet(pb))
+
+    for fn in _ICONS_24:
+        dir = os.path.join(pixmaps_dir,"24x24")
+        icon_filename = os.path.join(dir, fn + ".png")
         pb = gtk.gdk.pixbuf_new_from_file(icon_filename)
         factory.add(fn, gtk.IconSet(pb))
 
