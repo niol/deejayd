5ec831490b6f5ade85455306a30ff543860516e3
Mickael Royer <mickael.royer@gmail.com>
2008-01-02 09:55:46 +0100

    [deejayd] add licence header
    darcs-hash:20080102085546-b0006-d5807834ff9095ac20fb022452fa78412d6d601e.gz
    
    committer: Mickael Royer <mickael.royer@gmail.com>

--- src/deejayd/mediadb/__init__.py
+++ src/deejayd/mediadb/__init__.py
@@ -1,4 +1,20 @@
-# vim: ts=4 sw=4 expandtab
+# Deejayd, a media player daemon
+# Copyright (C) 2007 Mickael Royer <mickael.royer@gmail.com>
+#                    Alexandre Rossi <alexandre.rossi@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 from deejayd.ui import log
 from deejayd.mediadb import library,inotify
@@ -35,3 +51,5 @@ def init(db, player, config):
     else: log.info("inotify support disabled")
 
     return audio_library,video_library,lib_watcher
+
+# vim: ts=4 sw=4 expandtab
--- src/setup.py
+++ src/setup.py
@@ -1,5 +1,23 @@
 #!/usr/bin/env python
 
+# Deejayd, a media player daemon
+# Copyright (C) 2007 Mickael Royer <mickael.royer@gmail.com>
+#                    Alexandre Rossi <alexandre.rossi@gmail.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
 import glob,os
 from distutils.core import setup
 import deejayd
@@ -39,4 +57,5 @@ if __name__ == "__main__":
                          'deejayd.webui': ['templates/*.xml']},
            data_files= build_data_files_list(),
         )
+
 # vim: ts=4 sw=4 expandtab
