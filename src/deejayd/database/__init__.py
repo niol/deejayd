08795d93dc1bbf50f5387d1a7c3331beca75c1d7
Mickael Royer <mickael.royer@gmail.com>
2008-01-06 15:15:03 +0100

    [database] simplify database initialisation
    darcs-hash:20080106141503-b0006-0fc06eeb412d5a87966d8bc718bef46815388b5a.gz
    
    committer: Mickael Royer <mickael.royer@gmail.com>

--- src/deejayd/database/__init__.py
+++ src/deejayd/database/__init__.py
@@ -16,35 +16,24 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-class DatabaseFactory:
-    supported_database = ("sqlite")
-
-    def __init__(self,config):
-        self.config = config
-
-        try: self.db_type =  config.get("database","db_type")
-        except:
-            raise SystemExit(\
-                _("You do not choose a database. Verify your config file."))
-        else:
-            if self.db_type not in self.__class__.supported_database:
-                raise SystemExit(\
-     _("You chose a database which is not supported. Verify your config file."))
-
-    def get_db(self):
-        if self.db_type == "sqlite":
-            db_file = self.config.get("database","db_file")
-            try: prefix = self.config.get("database","db_prefix") + "_"
-            except NoOptionError: prefix = ""
-
-            from deejayd.database.sqlite import SqliteDatabase
-            return SqliteDatabase(db_file,prefix)
-
-        return None
-
+import sys
 
 def init(config):
-    database = DatabaseFactory(config)
+    try: db_type =  config.get("database","db_type")
+    except NoOptionError:
+        sys.exit(_("You do not choose a database. Verify your config file."))
+
+    if db_type == "sqlite":
+        db_file = config.get("database","db_file")
+        try: prefix = config.get("database","db_prefix") + "_"
+        except NoOptionError: prefix = ""
+
+        from deejayd.database.sqlite import SqliteDatabase
+        return SqliteDatabase(db_file,prefix)
+    else:
+        sys.exit(_("You chose a database which is not supported.\
+                    Verify your config file."))
+
     return database
 
 # vim: ts=4 sw=4 expandtab
