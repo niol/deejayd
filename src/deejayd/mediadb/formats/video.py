bd245be520ce440fcb8275000a0564e667419503
Alexandre Rossi <alexandre.rossi@gmail.com>
2008-03-06 23:39:08 +0100

    [mediadb] asf support
    darcs-hash:20080306223908-85ee7-82987dee9e694ce4c910c0e7f78bf46fb2c5aad1.gz
    
    committer: Alexandre Rossi <alexandre.rossi@gmail.com>

--- src/deejayd/mediadb/formats/video.py
+++ src/deejayd/mediadb/formats/video.py
@@ -17,7 +17,7 @@
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
 import os
-extensions = [".avi", ".mpeg", ".mpg", ".mkv"]
+extensions = (".avi", ".mpeg", ".mpg", ".mkv", ".asf", )
 
 class VideoFile:
     supported_tag = ("videowidth","length","videoheight")
