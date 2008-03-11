e174b8203fa134fc84889603be8f65b1a6371049
Alexandre Rossi <alexandre.rossi@gmail.com>
2008-03-11 07:48:28 +0100

    [player] new auto media backend which is the default and loads what is available'
    darcs-hash:20080311064828-85ee7-df78843f18caa86ff4e02d8b10236f9b0bbc326e.gz
    
    committer: Alexandre Rossi <alexandre.rossi@gmail.com>

--- src/deejayd/player/__init__.py
+++ src/deejayd/player/__init__.py
@@ -19,10 +19,34 @@
 import sys
 from deejayd.ui import log
 
+
+AVAILABLE_BACKENDS = ('xine', 'gstreamer', )
+
+
 class PlayerError(Exception):pass
 
 def init(db,config):
     media_backend = config.get("general","media_backend")
+
+    if media_backend == "auto":
+        backend_it = iter(AVAILABLE_BACKENDS)
+        media_backend = None
+        try:
+            while not media_backend:
+                backend = backend_it.next()
+                try:
+                    __import__('.'.join(('deejayd', 'player', backend, )))
+                except ImportError:
+                    # Do nothing, simply ignore
+                    pass
+                else:
+                    media_backend = backend
+                    config.set('general', 'mediabackend', backend)
+                    log.msg(_("Autodetected %s backend." % backend))
+        except StopIteration:
+            err = _("Could not find suitable media backend.")
+            log.err(err)
+            sys.exit(err)
 
     if media_backend == "gstreamer":
         from deejayd.player import gstreamer
