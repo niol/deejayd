851987a777650fd08937ba386401faee83cfedb9
Mickael Royer <mickael.royer@gmail.com>
2008-03-28 14:21:04 +0100

    [deejayd] prepare new release
    darcs-hash:20080328132104-b0006-958bbf855cca9644ad7be92824098a81bd8e83f7.gz
    
    committer: Mickael Royer <mickael.royer@gmail.com>

--- src/deejayd/__init__.py
+++ src/deejayd/__init__.py
@@ -16,6 +16,6 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-__version__ = "0.6.3"
+__version__ = "0.7.0"
 
 # vim: ts=4 sw=4 expandtab
