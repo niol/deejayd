3e712cf5a8ea70bf0db592f5698f6c6585b44f36
Mickael Royer <mickael.royer@gmail.com>
2008-05-14 23:36:06 +0200

    [mediadb] fix conflict with last patch
    darcs-hash:20080514213606-b0006-0a07443a498598cc3e9648c220be9dac13f1f4f7.gz
    
    committer: Mickael Royer <mickael.royer@gmail.com>

--- src/deejayd/mediadb/formats/audio_mp3.py
+++ src/deejayd/mediadb/formats/audio_mp3.py
@@ -16,7 +16,9 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-extensions = [".mp3",".mp2"]
+from deejayd.mediadb.formats._base import _AudioFile
+
+extensions = [".mp3",".mp2",".aac"]
 try: from mutagen.mp3 import MP3
 except ImportError:
     extensions = []
