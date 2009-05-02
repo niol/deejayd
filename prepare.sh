72ff46b9e5cfd54f044443306f5b4545acd62a05
Alexandre Rossi <alexandre.rossi@gmail.com>
2009-05-02 12:10:12 +0200

    do not assume that the maemosrc dir is there in prepare.sh as it is now part of deejayd dist
    Ignore-this: d5b4e3d0cfb90f8c0f77a3b5efe58619
    
    darcs-hash:20090502101012-85ee7-c744105af6260bc3fd767547d02bdee7cfeb1f54.gz
    
    committer: Alexandre Rossi <alexandre.rossi@gmail.com>

--- prepare.sh
+++ prepare.sh
@@ -10,14 +10,18 @@ makeexec scripts/testserver
 makeexec tests.py
 makeexec debian/rules
 
-makeexec maemosrc/debian/rules
 
-cd $SRCDIR/maemosrc/deejayd/
-ln -sf ../../deejayd/__init__.py .
-ln -sf ../../deejayd/interfaces.py .
-ln -sf ../../deejayd/xmlobject.py .
-ln -sf ../../deejayd/mediafilters.py .
-cd $SRCDIR/maemosrc/deejayd/net
-ln -sf ../../../deejayd/net/client.py .
-ln -sf ../../../deejayd/net/xmlbuilders.py .
-ln -sf ../../../deejayd/net/xmlparsers.py .
+if [ -d $SRCDIR/maemosrc ];
+then
+    makeexec maemosrc/debian/rules
+
+    cd $SRCDIR/maemosrc/deejayd/
+    ln -sf ../../deejayd/__init__.py .
+    ln -sf ../../deejayd/interfaces.py .
+    ln -sf ../../deejayd/xmlobject.py .
+    ln -sf ../../deejayd/mediafilters.py .
+    cd $SRCDIR/maemosrc/deejayd/net
+    ln -sf ../../../deejayd/net/client.py .
+    ln -sf ../../../deejayd/net/xmlbuilders.py .
+    ln -sf ../../../deejayd/net/xmlparsers.py .
+fi
