3df3d70d57821dd14116487903598fd7819eb494
Mickael Royer <mickael.royer@gmail.com>
2010-08-19 15:07:13 +0200

    [deejayd] remove maemosrc from this repository   * it is outdated and not maintened

--- prepare.sh
+++ prepare.sh
@@ -23,18 +23,3 @@ else
     echo "WARNING - deejayd webui can't be built"
     echo "WARNING - usage: prepare.sh GWT_LOCATION"
 fi
-
-if [ -d $SRCDIR/maemosrc ];
-then
-    makeexec maemosrc/debian/rules
-
-    cd $SRCDIR/maemosrc/deejayd/
-    ln -sf ../../deejayd/__init__.py .
-    ln -sf ../../deejayd/interfaces.py .
-    ln -sf ../../deejayd/mediafilters.py .
-    cd $SRCDIR/maemosrc/deejayd/rpc
-    ln -sf ../../../deejayd/rpc/jsonbuilders.py .
-    ln -sf ../../../deejayd/rpc/jsonparsers.py .
-    cd $SRCDIR/maemosrc/deejayd/net
-    ln -sf ../../../deejayd/net/client.py .
-fi
