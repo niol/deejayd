d98029546c26dbec2c9a2fa6d21ae81ccdc8a8ba
Mickaël Royer <mickael.royer@gmail.com>
2010-10-10 16:50:45 +0200

    [deejayd] fix prepare.sh script with new webui location

--- prepare.sh
+++ prepare.sh
@@ -15,9 +15,9 @@ if [ "$1" ];
 then
     python setup.py build_webui --gwt $1 --ant-target build
     cd $SRCDIR/data/htdocs/mobile/
-    ln -sf ../../../webui--gwt/deejayd-webui/war/mobile_webui .
+    ln -sf ../../../webui/war/mobile_webui .
     cd $SRCDIR/data/htdocs/webui/
-    ln -sf ../../../webui--gwt/deejayd-webui/war/deejayd_webui .
+    ln -sf ../../../webui/war/deejayd_webui .
 else
     echo "WARNING - You don't enter a GWT SDK Location"
     echo "WARNING - deejayd webui can't be built"
