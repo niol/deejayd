a4c90c26d3f4b4a041d35ea2f2fb22d21f613e81
Mickael Royer <mickael.royer@gmail.com>
2012-09-26 22:28:12 +0200

    [pytyxi] update ctypes function definitions to avoid potential segfault   * restype is c_int by default so explicitely set restype to None when     C function return nothing   * define opaque xine structure and POINTER(struct) in argstype and restype     definition

--- pytyxi/_xinelib.py
+++ pytyxi/_xinelib.py
@@ -29,30 +29,36 @@ try:
 except (ImportError, OSError), e:
     raise ImportError, e
 
+#
+# xine_t structure base definition
+#
+class xine_t(ctypes.Structure):
+    pass
 
 # void xine_get_version (int *major, int *minor, int *sub)
 _xinelib.xine_get_version.argstype = (ctypes.POINTER(ctypes.c_int),
                                       ctypes.POINTER(ctypes.c_int),
                                       ctypes.POINTER(ctypes.c_int),)
+_xinelib.xine_get_version.restype = None
 
 # int  xine_check_version (int major, int minor, int sub)
 _xinelib.xine_check_version.argtypes = (ctypes.c_int, ctypes.c_int,
                                         ctypes.c_int)
-_xinelib.xine_check_version.restype = ctypes.c_int
 
 # char *xine_get_file_extensions (xine_t *self)
-_xinelib.xine_get_file_extensions.argtypes = (ctypes.c_void_p, )
+_xinelib.xine_get_file_extensions.argtypes = (ctypes.POINTER(xine_t), )
 _xinelib.xine_get_file_extensions.restype = ctypes.c_char_p
 
 # char *const *xine_list_input_plugins(xine_t *self)
-_xinelib.xine_list_input_plugins.argtypes = (ctypes.c_void_p, )
+_xinelib.xine_list_input_plugins.argtypes = (ctypes.POINTER(xine_t), )
 _xinelib.xine_list_input_plugins.restype = ctypes.POINTER(ctypes.c_char_p)
 
 # xine_t *xine_new (void)
-_xinelib.xine_new.restype = ctypes.c_void_p
+_xinelib.xine_new.restype = ctypes.POINTER(xine_t)
 
 # void xine_config_load  (xine_t *self, const char *cfg_filename)
-_xinelib.xine_config_load.argstype = (ctypes.c_void_p, ctypes.c_char_p)
+_xinelib.xine_config_load.argstype = (ctypes.POINTER(xine_t), ctypes.c_char_p)
+_xinelib.xine_config_load.restype = None
 
 class xine_cfg_entry_t(ctypes.Structure):
     _fields_ = (
@@ -66,7 +72,7 @@ class xine_cfg_entry_t(ctypes.Structure):
         ('num_default', ctypes.c_int),
         ('range_min', ctypes.c_int),
         ('range_max', ctypes.c_int),
-        ('enum_values', ctypes.c_void_p), # char **enum_values
+        ('enum_values', ctypes.POINTER(ctypes.c_char_p)), # char **enum_values
         ('description', ctypes.c_char_p),
         ('help', ctypes.c_char_p),
         ('callback', ctypes.c_void_p),
@@ -74,89 +80,111 @@ class xine_cfg_entry_t(ctypes.Structure):
     )
 
 # int  xine_config_get_first_entry (xine_t *self, xine_cfg_entry_t *entry)
-_xinelib.xine_config_get_first_entry.argstype = (ctypes.c_void_p,
-                                                 ctypes.c_void_p, )
-_xinelib.xine_config_get_first_entry.restype = ctypes.c_int
+_xinelib.xine_config_get_first_entry.argstype = (ctypes.POINTER(xine_t),
+                                             ctypes.POINTER(xine_cfg_entry_t), )
 
 # int  xine_config_get_next_entry (xine_t *self, xine_cfg_entry_t *entry)
-_xinelib.xine_config_get_next_entry.argstype = (ctypes.c_void_p,
-                                                ctypes.c_void_p, )
-_xinelib.xine_config_get_next_entry.restype = ctypes.c_int
+_xinelib.xine_config_get_next_entry.argstype = (ctypes.POINTER(xine_t),
+                                             ctypes.POINTER(xine_cfg_entry_t), )
 
 # int  xine_config_lookup_entry (xine_t *self, const char *key,
 #                  xine_cfg_entry_t *entry)
-_xinelib.xine_config_lookup_entry.argstype = (ctypes.c_void_p,
-                                              ctypes.c_char_p,
-                                              ctypes.c_void_p, )
-_xinelib.xine_config_lookup_entry.restype = ctypes.c_int
+_xinelib.xine_config_lookup_entry.argstype = (ctypes.POINTER(xine_t),
+                                          ctypes.c_char_p,
+                                          ctypes.POINTER(xine_cfg_entry_t), )
 
 # void xine_config_update_entry (xine_t *self,
 #                   const xine_cfg_entry_t *entry)
-_xinelib.xine_config_update_entry.argstype = (ctypes.c_void_p,
-                                              ctypes.c_void_p, )
+_xinelib.xine_config_update_entry.argstype = (ctypes.POINTER(xine_t),
+                                          ctypes.POINTER(xine_cfg_entry_t), )
+_xinelib.xine_config_update_entry.restype = None
 
 # const char *xine_get_homedir(void)
 _xinelib.xine_get_homedir.restype = ctypes.c_char_p
 
 # void xine_init (xine_t *self)
-_xinelib.xine_init.argstype = (ctypes.c_void_p, )
+_xinelib.xine_init.argstype = (ctypes.POINTER(xine_t), )
+_xinelib.xine_init.restype = None
 
 # void xine_exit (xine_t *self)
-_xinelib.xine_exit.argstype = (ctypes.c_void_p, )
+_xinelib.xine_exit.argstype = (ctypes.POINTER(xine_t), )
+_xinelib.xine_exit.restype = None
 
 # void xine_engine_set_param(xine_t *self, int param, int value)
-_xinelib.xine_engine_set_param.argstype = (ctypes.c_void_p,
+_xinelib.xine_engine_set_param.argstype = (ctypes.POINTER(xine_t),
                                            ctypes.c_int, ctypes.c_int, )
+_xinelib.xine_engine_set_param.restype = None
 
+#
+# xine_audio_port_t and xine_video_port_t structure base definition
+#
+class xine_audio_port_t(ctypes.Structure):
+    pass
+class xine_video_port_t(ctypes.Structure):
+    pass
 # xine_audio_port_t *xine_open_audio_driver (xine_t *self, const char *id,
 #                       void *data)
-_xinelib.xine_open_audio_driver.argstype = (ctypes.c_void_p,
+_xinelib.xine_open_audio_driver.argstype = (ctypes.POINTER(xine_t),
                                             ctypes.c_char_p, ctypes.c_void_p, )
-_xinelib.xine_open_audio_driver.restype = ctypes.c_void_p
+_xinelib.xine_open_audio_driver.restype = ctypes.POINTER(xine_audio_port_t)
 
 # xine_video_port_t *xine_open_video_driver (xine_t *self, const char *id,
 #                       int visual, void *data)
-_xinelib.xine_open_video_driver.argstype = (ctypes.c_void_p,
+_xinelib.xine_open_video_driver.argstype = (ctypes.POINTER(xine_t),
                                             ctypes.c_char_p,
                                             ctypes.c_int, ctypes.c_void_p, )
-_xinelib.xine_open_video_driver.restype = ctypes.c_void_p
+_xinelib.xine_open_video_driver.restype = ctypes.POINTER(xine_video_port_t)
 
 # void xine_close_audio_driver (xine_t *self, xine_audio_port_t  *driver)
-_xinelib.xine_close_audio_driver.argstype = (ctypes.c_void_p, ctypes.c_void_p, )
+_xinelib.xine_close_audio_driver.argstype = (ctypes.POINTER(xine_t),
+                                             ctypes.POINTER(xine_audio_port_t),)
+_xinelib.xine_close_audio_driver.restype = None
 
 # void xine_close_video_driver (xine_t *self, xine_video_port_t  *driver)
-_xinelib.xine_close_video_driver.argstype = (ctypes.c_void_p, ctypes.c_void_p, )
+_xinelib.xine_close_video_driver.argstype = (ctypes.POINTER(xine_t),
+                                             ctypes.POINTER(xine_video_port_t),)
+_xinelib.xine_close_video_driver.restype = None
 
 # int    xine_port_send_gui_data (xine_video_port_t *vo,
 #                    int type, void *data)
-_xinelib.xine_port_send_gui_data.argstype = (ctypes.c_void_p,
+_xinelib.xine_port_send_gui_data.argstype = (ctypes.POINTER(xine_video_port_t),
                                              ctypes.c_int, ctypes.c_void_p, )
-_xinelib.xine_port_send_gui_data.restype = ctypes.c_int
 
+
+#
+# Stream structure base definition
+#
+class xine_stream_t(ctypes.Structure):
+    pass
 # xine_stream_t *xine_stream_new (xine_t *self,
 #                xine_audio_port_t *ao, xine_video_port_t *vo)
-_xinelib.xine_stream_new.argstype = (ctypes.c_void_p,
-                                     ctypes.c_void_p, ctypes.c_void_p,)
-_xinelib.xine_stream_new.restype = ctypes.c_void_p
+_xinelib.xine_stream_new.argstype = (ctypes.POINTER(xine_t),
+                                     ctypes.POINTER(xine_audio_port_t),
+                                     ctypes.POINTER(xine_video_port_t),)
+_xinelib.xine_stream_new.restype = ctypes.POINTER(xine_stream_t)
 
 # int xine_open (xine_stream_t *stream, const char *mrl)
-_xinelib.xine_open.argstype = (ctypes.c_void_p, ctypes.c_char_p, )
-_xinelib.xine_open.restype = ctypes.c_int
+_xinelib.xine_open.argstype = (ctypes.POINTER(xine_stream_t), ctypes.c_char_p, )
 
 # int  xine_play (xine_stream_t *stream, int start_pos, int start_time)
-_xinelib.xine_play.argstype = (ctypes.c_void_p, ctypes.c_int, ctypes.c_int, )
-_xinelib.xine_play.restype = ctypes.c_int
+_xinelib.xine_play.argstype = (ctypes.POINTER(xine_stream_t), ctypes.c_int,
+                               ctypes.c_int, )
 
 # void xine_stop (xine_stream_t *stream)
-_xinelib.xine_stop.argstype = (ctypes.c_void_p, )
+_xinelib.xine_stop.argstype = (ctypes.POINTER(xine_stream_t), )
+_xinelib.xine_stop.restype = None
 
+# void xine_usec_sleep (int usec)
 _xinelib.xine_usec_sleep.argtypes = (ctypes.c_int, )
+_xinelib.xine_usec_sleep.restype = None
 
 # void xine_close (xine_stream_t *stream)
-_xinelib.xine_close.argstype = (ctypes.c_void_p, )
+_xinelib.xine_close.argstype = (ctypes.POINTER(xine_stream_t), )
+_xinelib.xine_close.restype = None
 
 # void xine_dispose (xine_stream_t *stream)
-_xinelib.xine_dispose.argstype = (ctypes.c_void_p, )
+_xinelib.xine_dispose.argstype = (ctypes.POINTER(xine_stream_t), )
+_xinelib.xine_dispose.restype = None
 
 class x11_visual_t(ctypes.Structure):
     _fields_ = (
@@ -221,104 +249,118 @@ class xine_ui_message_data_t(ctypes.Structure):
 xine_event_listener_cb_t = ctypes.CFUNCTYPE(ctypes.c_void_p, ctypes.c_void_p,
                                             ctypes.POINTER(xine_event_t))
 
+# Opaque xine_event_queue_t structure
+class xine_event_queue_t(ctypes.Structure):
+    pass
 # void xine_event_create_listener_thread(xine_event_queue_t *queue,
 #    xine_event_listener_cb_t callback,
 #    void *user_data)
-_xinelib.xine_event_create_listener_thread.argtypes = (ctypes.c_void_p,
-                                                       ctypes.c_void_p,
-                                                       ctypes.c_void_p)
+_xinelib.xine_event_create_listener_thread.argtypes = (\
+                                       ctypes.POINTER(xine_event_queue_t),
+                                       ctypes.c_void_p,
+                                       ctypes.c_void_p)
+_xinelib.xine_event_create_listener_thread.restype = None
 
 # xine_event_queue_t *xine_event_new_queue(xine_stream_t *stream)
-_xinelib.xine_event_new_queue.argtypes = (ctypes.c_void_p, )
-_xinelib.xine_event_new_queue.restype = ctypes.c_void_p
+_xinelib.xine_event_new_queue.argtypes = (ctypes.POINTER(xine_stream_t), )
+_xinelib.xine_event_new_queue.restype = ctypes.POINTER(xine_event_queue_t)
 
 # void xine_event_dispose_queue(xine_event_queue_t *queue)
-_xinelib.xine_event_dispose_queue.argtypes = (ctypes.c_void_p, )
+_xinelib.xine_event_dispose_queue.argtypes = \
+        (ctypes.POINTER(xine_event_queue_t), )
+_xinelib.xine_event_dispose_queue.restype = None
 
 # void xine_set_param (xine_stream_t *stream, int param, int value)
-_xinelib.xine_set_param.argtypes = (ctypes.c_void_p, ctypes.c_int, ctypes.c_int)
+_xinelib.xine_set_param.argtypes = (ctypes.POINTER(xine_stream_t),
+                                    ctypes.c_int, ctypes.c_int)
+_xinelib.xine_set_param.restype = None
 
 # int xine_get_param (xine_stream_t *stream, int param)
-_xinelib.xine_get_param.argtypes = (ctypes.c_void_p, ctypes.c_int)
-_xinelib.xine_get_param.restype = ctypes.c_int
+_xinelib.xine_get_param.argtypes = (ctypes.POINTER(xine_stream_t), ctypes.c_int)
 
 # char *xine_get_meta_info(xine_stream_t *stream, int info)
-_xinelib.xine_get_meta_info.argtypes = (ctypes.c_void_p, ctypes.c_int)
+_xinelib.xine_get_meta_info.argtypes = (ctypes.POINTER(xine_stream_t),
+                                        ctypes.c_int)
 _xinelib.xine_get_meta_info.restype = ctypes.c_char_p
 
 # int xine_get_stream_info(xine_stream_t *stream, int info)
-_xinelib.xine_get_stream_info.argtypes = (ctypes.c_void_p, ctypes.c_int)
-_xinelib.xine_get_stream_info.restype = ctypes.c_int
+_xinelib.xine_get_stream_info.argtypes = (ctypes.POINTER(xine_stream_t),
+                                          ctypes.c_int)
 
 # int xine_get_status (xine_stream_t *stream)
-_xinelib.xine_get_status.argtypes = (ctypes.c_void_p, )
-_xinelib.xine_get_status.restype = ctypes.c_int
+_xinelib.xine_get_status.argtypes = (ctypes.POINTER(xine_stream_t), )
 
 # int xine_get_pos_length (xine_stream_t *stream, int *pos_stream,
 #                          int *pos_time, int *length_time)
-_xinelib.xine_get_pos_length.argtypes = (ctypes.c_void_p,
+_xinelib.xine_get_pos_length.argtypes = (ctypes.POINTER(xine_stream_t),
                                          ctypes.POINTER(ctypes.c_int),
                                          ctypes.POINTER(ctypes.c_int),
                                          ctypes.POINTER(ctypes.c_int))
 
-# int xine_get_status (xine_stream_t *stream)
-_xinelib.xine_get_status.argtypes = (ctypes.c_void_p, )
-_xinelib.xine_get_status.restype = ctypes.c_int
-
 # int xine_get_audio_lang(xine_stream_t *stream, int channel, char *lang)
-_xinelib.xine_get_audio_lang.restype = ctypes.c_int
-_xinelib.xine_get_audio_lang.argtypes = (ctypes.c_void_p, ctypes.c_int,
-                                         ctypes.c_char_p)
+_xinelib.xine_get_audio_lang.argtypes = (ctypes.POINTER(xine_stream_t),
+                                         ctypes.c_int, ctypes.c_char_p)
 
 # int xine_get_spu_lang(xine_stream_t *stream, int channel, char *lang)
-_xinelib.xine_get_spu_lang.restype = ctypes.c_int
-_xinelib.xine_get_spu_lang.argtypes = (ctypes.c_void_p, ctypes.c_int,
-                                       ctypes.c_char_p)
+_xinelib.xine_get_spu_lang.argtypes = (ctypes.POINTER(xine_stream_t),
+                                       ctypes.c_int, ctypes.c_char_p)
 
+# Opaque xine_osd_t structure definition
+class xine_osd_t(ctypes.Structure):
+    pass
 # xine_osd_t *xine_osd_new(xine_stream_t *self, int x, int y,
 #                          int width, int height)
-_xinelib.xine_osd_new.argtypes = (ctypes.c_void_p, ctypes.c_int, ctypes.c_int,
+_xinelib.xine_osd_new.argtypes = (ctypes.POINTER(xine_stream_t),
+                                  ctypes.c_int, ctypes.c_int,
                                   ctypes.c_int, ctypes.c_int)
-_xinelib.xine_osd_new.restype = ctypes.c_void_p
+_xinelib.xine_osd_new.restype = ctypes.POINTER(xine_osd_t)
 
 # void xine_osd_free(xine_osd_t *self)
-_xinelib.xine_osd_free.argtypes = (ctypes.c_void_p, )
+_xinelib.xine_osd_free.argtypes = (ctypes.POINTER(xine_osd_t), )
+_xinelib.xine_osd_free.restype = None
 
 # uint32_t xine_osd_get_capabilities(xine_osd_t *self)
-_xinelib.xine_osd_get_capabilities.restype = ctypes.c_int
-_xinelib.xine_osd_get_capabilities.argtypes = (ctypes.c_void_p, )
+_xinelib.xine_osd_get_capabilities.argtypes = (ctypes.POINTER(xine_osd_t), )
+_xinelib.xine_osd_get_capabilities.restype = ctypes.c_uint
 
 # void xine_osd_set_text_palette(xine_osd_t *self,int palette_number,
 #                                int color_base )
-_xinelib.xine_osd_set_text_palette.argtypes = (ctypes.c_void_p, ctypes.c_int,
-                                               ctypes.c_int)
+_xinelib.xine_osd_set_text_palette.argtypes = (ctypes.POINTER(xine_osd_t),
+                                               ctypes.c_int, ctypes.c_int)
 
 # int xine_osd_set_font(xine_osd_t *self, const char *fontname, int size)
-_xinelib.xine_osd_set_font.restype = ctypes.c_int
-_xinelib.xine_osd_set_font.argtypes = (ctypes.c_void_p, ctypes.c_char_p,
-                                       ctypes.c_int)
+_xinelib.xine_osd_set_font.argtypes = (ctypes.POINTER(xine_osd_t),
+                                       ctypes.c_char_p, ctypes.c_int)
 
 # void xine_osd_set_position(xine_osd_t *self, int x, int y)
-_xinelib.xine_osd_set_position.argtypes = (ctypes.c_void_p, ctypes.c_int,
-                                        ctypes.c_int)
+_xinelib.xine_osd_set_position.argtypes = (ctypes.POINTER(xine_osd_t),
+                                           ctypes.c_int, ctypes.c_int)
+_xinelib.xine_osd_set_position.restype = None
 
 # void xine_osd_draw_text(xine_osd_t *self, int x1, int y1, char *text,
 #                         int color_base)
-_xinelib.xine_osd_draw_text.argtypes = (ctypes.c_void_p, ctypes.c_int,
+_xinelib.xine_osd_draw_text.argtypes = (ctypes.POINTER(xine_osd_t),
+                                        ctypes.c_int,
                                         ctypes.c_int, ctypes.c_char_p,
                                         ctypes.c_int)
+_xinelib.xine_osd_draw_text.restype = None
 
 # void xine_osd_show(xine_osd_t *self, int64_t vpts)
-_xinelib.xine_osd_show.argtypes = (ctypes.c_void_p, ctypes.c_int)
+_xinelib.xine_osd_show.argtypes = (ctypes.POINTER(xine_osd_t), ctypes.c_int)
+_xinelib.xine_osd_show.restype = None
 
 # void  xine_osd_show_unscaled (xine_osd_t *self, int64_t vpts)
-_xinelib.xine_osd_show_unscaled.argtypes = (ctypes.c_void_p, ctypes.c_int)
+_xinelib.xine_osd_show_unscaled.argtypes = (ctypes.POINTER(xine_osd_t),
+                                            ctypes.c_int)
+_xinelib.xine_osd_show_unscaled.restype = None
 
 # void  xine_osd_hide(xine_osd_t *self, int64_t vpts)
-_xinelib.xine_osd_hide.argtypes = (ctypes.c_void_p, ctypes.c_int)
+_xinelib.xine_osd_hide.argtypes = (ctypes.POINTER(xine_osd_t), ctypes.c_int)
+_xinelib.xine_osd_hide.restype = None
 
 # void  xine_osd_clear(xine_osd_t *self)
-_xinelib.xine_osd_clear.argtypes = (ctypes.c_void_p, )
+_xinelib.xine_osd_clear.argtypes = (ctypes.POINTER(xine_osd_t), )
+_xinelib.xine_osd_clear.restype = None
 
 
 # copy functions from the library
